9f4466f2992a7ff33b76a9c666dc760815c67ff7
Ricardo Valerio <jricvalerio@gmail.com>
2023-11-25 13:36:58 +0000

    added type annotations

--- Species.py
+++ Species.py
@@ -1,5 +1,11 @@
 class Species:
-    def __init__(self, nome, tipo_folhagem, produz_fruto, tipo_planta, raio_max, num_medio_anos_vida):
+    def __init__(self,
+                 nome: str,
+                 tipo_folhagem: str,
+                 produz_fruto: bool,
+                 tipo_planta: str,
+                 raio_max: float,
+                 num_medio_anos_vida: int):
         self.__nome = nome
         self.__tipo_folhagem = tipo_folhagem
         self.__produz_fruto = produz_fruto
@@ -8,30 +14,29 @@ class Species:
         self.__num_medio_anos_vida = num_medio_anos_vida
 
     @property
-    def nome(self):
+    def nome(self) -> str:
         return self.__nome
 
     @property
-    def tipo_folhagem(self):
+    def tipo_folhagem(self) -> str:
         return self.__tipo_folhagem
 
     @property
-    def produz_fruto(self):
+    def produz_fruto(self) -> bool:
         return self.__produz_fruto
 
     @property
-    def tipo_planta(self):
+    def tipo_planta(self) -> str:
         return self.__tipo_planta
 
     @property
-    def raio_max(self):
+    def raio_max(self) -> float:
         return self.__raio_max
 
     @property
-    def num_medio_anos_vida(self):
+    def num_medio_anos_vida(self) -> int:
         return self.__num_medio_anos_vida
 
     @property
-    def area_de_ocupacao_circular(self):
+    def area_de_ocupacao_circular(self) -> float:
         return 3.14 * (self.__raio_max ** 2)
-
