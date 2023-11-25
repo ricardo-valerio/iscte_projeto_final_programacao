479e0119e4acc37a96f61f3e3050d21841b8dab1
Ricardo Valerio <jricvalerio@gmail.com>
2023-11-25 18:26:42 +0000

    método "pertence_a_area_de_ocupacao_plantacao" da class Plant

--- Plant.py
+++ Plant.py
@@ -23,8 +23,14 @@ class Plant:
             return None
         return ano_a_verificar - self.__ano_plantacao
 
+    def pertence_a_area_de_ocupacao_plantacao(self, dadas_as_coordenadas):
+        distancia = math_sqrt(
+            (self.localizacao_coords[0] - dadas_as_coordenadas[0])**2 +
+            (self.localizacao_coords[1] - dadas_as_coordenadas[1])**2
+        )
+        return distancia < self.__especie.raio_max
+
     def __str__(self):
         return f"Nome da planta: {self.__especie.nome}\n"      \
                f"Localização GPS: {self.localizacao_coords}\n" \
                f"Ano de plantação: {self.__ano_plantacao}\n"
-
